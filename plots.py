a9afe49f9053ff61e8aec0abe2d456eae936b978
Hao Wu <z5132458@unsw.edu.au>
2019-04-05 14:27:30 +1100

    convert indentation to 4 spaces instead of a tab compare with last version

--- plots.py
+++ plots.py
@@ -9,9 +9,9 @@ import seaborn as sns
 
 ##heat map to show the variable correlation
 def heatmap():
-	frame=clean_data()
-	fig=plt.figure(figsize=(10,10))
-	plt.suptitle('heatmap')
-	sns.heatmap(frame.corr(),annot=True,vmax=0.3,linewidths=0.5,annot_kws={"size": 10})
-	plt.show()
-	fig.savefig('heatmap.png')
+    frame=clean_data()
+    fig=plt.figure(figsize=(10,10))
+    plt.suptitle('heatmap')
+    sns.heatmap(frame.corr(),annot=True,vmax=0.3,linewidths=0.5,annot_kws={"size": 10})
+    plt.show()
+    fig.savefig('heatmap.png')
