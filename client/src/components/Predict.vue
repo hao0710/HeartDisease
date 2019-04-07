f8fb204c4d5587c0cbba83137f3537539b036044
hanxuwu <skunder301@gmail.com>
2019-04-07 23:41:06 +1000

    #feature-visualize# beautify the predict page

--- client/src/components/Predict.vue
+++ client/src/components/Predict.vue
@@ -7,245 +7,256 @@
       lead="Predict heart disease using machine learning"
     ></b-jumbotron>
     <!--<p>{{ data }}</p>-->
+    <div class="row">
+      <div class="col-md-6">
+        <div class="tile">
+          <div class="container">
+            <h4>Please enter information below</h4>
+            <hr>
+            <h5 class="c-form-label" for="gender">Gender</h5>
+            <b-form-radio-group
+              id="btn-radios-1"
+              v-model="selected"
+              :options="options_1"
+              buttons
+              name="radios-btn-default"
+            ></b-form-radio-group>
+            <!-- <p>
+              Pressed State:
+              <strong>{{selected}}</strong>
+            </p>-->
+            <!--Age--->
 
-    <!--Gender selector -->
-    <div class="container">
-      <h2>Please enter information below</h2>
-      <hr>
-      <h3 class="c-form-label" for="gender">Gender</h3>
-      <br>
-      <b-form-radio-group
-        id="btn-radios-1"
-        v-model="selected"
-        :options="options_1"
-        buttons
-        name="radios-btn-default"
-      ></b-form-radio-group>
-      <p>
-        Pressed State:
-        <strong>{{selected}}</strong>
-      </p>
-      <!--Age--->
+            <div role="group">
+              <h5 for="input-live">Age:</h5>
+              <b-form-input
+                id="input-live"
+                v-model="age"
+                :state="ageState"
+                aria-describedby="input-live-help input-live-feedback"
+                placeholder="Enter your age"
+                trim
+              ></b-form-input>
 
-      <div role="group">
-        <h3 for="input-live">Age:</h3>
-        <b-form-input
-          id="input-live"
-          v-model="age"
-          :state="ageState"
-          aria-describedby="input-live-help input-live-feedback"
-          placeholder="Enter your age"
-          trim
-        ></b-form-input>
+              <!-- This will only be shown if the preceding input has an invalid state -->
+              <b-form-invalid-feedback
+                id="input-live-feedback"
+              >Please enter an age beteen 35-74 years</b-form-invalid-feedback>
 
-        <!-- This will only be shown if the preceding input has an invalid state -->
-        <b-form-invalid-feedback id="input-live-feedback">Please enter an age beteen 35-74 years</b-form-invalid-feedback>
+              <!-- This is a form text block (formerly known as help block) -->
+              <!--<b-form-text id="input-live-help">Your age.</b-form-text>-->
+              <!-- <p>
+                Input Age:
+                <strong>{{age}}</strong>
+              </p>-->
+            </div>
 
-        <!-- This is a form text block (formerly known as help block) -->
-        <!--<b-form-text id="input-live-help">Your age.</b-form-text>-->
-        <p>
-          Input Age:
-          <strong>{{age}}</strong>
-        </p>
-      </div>
+            <!--Chest Pain Type--->
+            <h5 class="c-form-label" for="Chest pain type">Chest Pain Type</h5>
+            <div>
+              <b-form-radio-group
+                id="radio-group-1"
+                v-model="selected_pain"
+                :options="options_2"
+                name="radio-options"
+              ></b-form-radio-group>
+              <!-- 
+              <div class="mt-3">
+                Selected:
+                <strong>{{ selected_pain }}</strong>
+              </div>-->
+            </div>
 
-      <!--Chest Pain Type--->
-      <h3 class="c-form-label" for="Chest pain type">Chest Pain Type</h3>
-      <div>
-        <b-form-radio-group
-          id="radio-group-1"
-          v-model="selected_pain"
-          :options="options_2"
-          name="radio-options"
-        ></b-form-radio-group>
+            <!--Resting Blood Pressure--->
 
-        <div class="mt-3">
-          Selected:
-          <strong>{{ selected_pain }}</strong>
+            <div role="group">
+              <h5 for="input-live">Resting Blood Pressure:</h5>
+              <b-form-input
+                id="input-live"
+                v-model="bloodpressure"
+                :state="bloodpressureState"
+                aria-describedby="input-live-help input-live-feedback"
+                placeholder="Enter your Resting Blood Pressure"
+                trim
+              ></b-form-input>
+
+              <!-- This will only be shown if the preceding input has an invalid state -->
+              <b-form-invalid-feedback
+                id="input-live-feedback"
+              >Please enter Resting Blood Pressure between 0-300</b-form-invalid-feedback>
+
+              <!-- This is a form text block (formerly known as help block) -->
+              <!--<b-form-text id="input-live-help">Your age.</b-form-text>-->
+              <!-- <p>
+                Input blood pressure:
+                <strong>{{bloodpressure}}</strong>
+              </p>-->
+            </div>
+
+            <!-- Serum Cholestoral in mg/dl -->
+            <div role="group">
+              <h5 for="input-live">Serum Cholestoral in mg/dl:</h5>
+              <b-form-input
+                id="input-live"
+                v-model="cholestoral"
+                :state="cholestoralState"
+                aria-describedby="input-live-help input-live-feedback"
+                placeholder="Enter your Serum Cholestoral in mg/dl"
+                trim
+              ></b-form-input>
+
+              <!-- This will only be shown if the preceding input has an invalid state -->
+              <b-form-invalid-feedback
+                id="input-live-feedback"
+              >Please enter Serum Cholestoral in mg/dl</b-form-invalid-feedback>
+
+              <!-- This is a form text block (formerly known as help block) -->
+              <!--<b-form-text id="input-live-help">Your age.</b-form-text>-->
+              <!-- <p>
+                Input cholestoral:
+                <strong>{{cholestoral}}</strong>
+              </p>-->
+            </div>
+
+            <!-- Fasting Blood Sugar > 120 mg/dl-->
+
+            <h5 class="c-form-label" for="bloodsugar">Fasting Blood Sugar > 120 mg/dl</h5>
+            <b-form-radio-group
+              id="btn-radios-1"
+              v-model="selected_bloodsugar"
+              :options="options_3"
+              buttons
+              name="radios-btn-default"
+            ></b-form-radio-group>
+            <!-- <p>
+              bloodsugar State:
+              <strong>{{selected_bloodsugar}}</strong>
+            </p>-->
+
+            <!--Resting Electrocardiographic Results-->
+
+            <h5
+              class="c-form-label"
+              for="Resting Electrocardiographic Results"
+            >Resting Electrocardiographic Results</h5>
+            <div>
+              <b-form-radio-group
+                id="radio-group-2"
+                v-model="selected_electrocardiographic"
+                :options="options_4"
+                name="radio-options1"
+              ></b-form-radio-group>
+              <!-- 
+              <div class="mt-3">
+                Selected:
+                <strong>{{ selected_electrocardiographic }}</strong>
+              </div>-->
+            </div>
+
+            <!--Maximum Heart Rate Achieved-->
+            <div role="group">
+              <h5 for="input-live">Maximum Heart Rate Achieved:</h5>
+              <b-form-input
+                id="input-live"
+                v-model="heart_rate"
+                :state="heart_rateState"
+                aria-describedby="input-live-help input-live-feedback"
+                placeholder="Enter your Maximum Heart Rate Achieved"
+                trim
+              ></b-form-input>
+
+              <!-- This will only be shown if the preceding input has an invalid state -->
+              <b-form-invalid-feedback
+                id="input-live-feedback"
+              >Please enter Maximum Heart Rate Achieved</b-form-invalid-feedback>
+
+              <!-- This is a form text block (formerly known as help block) -->
+              <!--<b-form-text id="input-live-help">Your age.</b-form-text>-->
+              <!-- <p>
+                Input max heart rate:
+                <strong>{{heart_rate}}</strong>
+              </p>-->
+            </div>
+
+            <!-- Exercise Induced Angina -->
+            <h5 class="c-form-label" for="angina">Exercise Induced Angina</h5>
+            <!-- <br> -->
+            <b-form-radio-group
+              id="btn-radios-1"
+              v-model="selected_angina"
+              :options="options_5"
+              buttons
+              name="radios-btn-default"
+            ></b-form-radio-group>
+            <!-- <p>
+              Exercise angina State:
+              <strong>{{selected_angina}}</strong>
+            </p>-->
+
+            <!-- Old peak  ST depression induced by exercise relative to rest -->
+            <div role="group">
+              <h5 for="input-live">Old peak:ST depression induced by exercise relative to rest:</h5>
+              <b-form-input
+                id="input-live"
+                v-model="oldpeak"
+                :state="oldpeakState"
+                aria-describedby="input-live-help input-live-feedback"
+                placeholder="Enter your Old peak:ST depression induced by exercise relative to rest"
+                trim
+              ></b-form-input>
+
+              <!-- This will only be shown if the preceding input has an invalid state -->
+              <b-form-invalid-feedback
+                id="input-live-feedback"
+              >Please enter Old peak:ST depression induced by exercise relative to rest</b-form-invalid-feedback>
+
+              <!-- This is a form text block (formerly known as help block) -->
+              <!--<b-form-text id="input-live-help">Your age.</b-form-text>-->
+              <!-- <p>
+                Input oldpeak:
+                <strong>{{oldpeak}}</strong>
+              </p>-->
+            </div>
+            <!-- The Slope of the Peak Exercise ST segment -->
+
+            <h5 class="c-form-label" for="slope">The Slope of the Peak Exercise ST segment</h5>
+            <!-- <br> -->
+            <b-form-radio-group
+              id="btn-radios-1"
+              v-model="selected_slope"
+              :options="options_6"
+              buttons
+              name="radios-btn-default"
+            ></b-form-radio-group>
+            <!-- <p>
+              Slope of Peak State:
+              <strong>{{selected_slope}}</strong>
+            </p>-->
+
+            <!-- ca: number of major vessels (0-3) colored by flourosopy  -->
+            <h5 class="c-form-label" for="ca">Number of major vessels (0-3) colored by flourosopy</h5>
+            <!-- <br> -->
+            <b-form-radio-group
+              id="btn-radios-1"
+              v-model="selected_ca"
+              :options="options_7"
+              buttons
+              name="radios-btn-default"
+            ></b-form-radio-group>
+            <!-- <p>
+              Number of major vessels:
+              <strong>{{selected_ca}}</strong>
+            </p>-->
+          </div>
+          <br>
+          <div class="aaa">
+          <b-button @click="onSubmit" type="submit" variant="info">Submit</b-button>
+          </div>
+          <br>
         </div>
       </div>
-
-      <!--Resting Blood Pressure--->
-
-      <div role="group">
-        <h3 for="input-live">Resting Blood Pressure:</h3>
-        <b-form-input
-          id="input-live"
-          v-model="bloodpressure"
-          :state="bloodpressureState"
-          aria-describedby="input-live-help input-live-feedback"
-          placeholder="Enter your Resting Blood Pressure"
-          trim
-        ></b-form-input>
-
-        <!-- This will only be shown if the preceding input has an invalid state -->
-        <b-form-invalid-feedback
-          id="input-live-feedback"
-        >Please enter Resting Blood Pressure between 0-300</b-form-invalid-feedback>
-
-        <!-- This is a form text block (formerly known as help block) -->
-        <!--<b-form-text id="input-live-help">Your age.</b-form-text>-->
-        <p>
-          Input blood pressure:
-          <strong>{{bloodpressure}}</strong>
-        </p>
-      </div>
-
-      <!-- Serum Cholestoral in mg/dl -->
-      <div role="group">
-        <h3 for="input-live">Serum Cholestoral in mg/dl:</h3>
-        <b-form-input
-          id="input-live"
-          v-model="cholestoral"
-          :state="cholestoralState"
-          aria-describedby="input-live-help input-live-feedback"
-          placeholder="Enter your Serum Cholestoral in mg/dl"
-          trim
-        ></b-form-input>
-
-        <!-- This will only be shown if the preceding input has an invalid state -->
-        <b-form-invalid-feedback id="input-live-feedback">Please enter Serum Cholestoral in mg/dl</b-form-invalid-feedback>
-
-        <!-- This is a form text block (formerly known as help block) -->
-        <!--<b-form-text id="input-live-help">Your age.</b-form-text>-->
-        <p>
-          Input cholestoral:
-          <strong>{{cholestoral}}</strong>
-        </p>
-      </div>
-
-      <!-- Fasting Blood Sugar > 120 mg/dl-->
-
-      <h3 class="c-form-label" for="bloodsugar">Fasting Blood Sugar > 120 mg/dl</h3>
-      <br>
-      <b-form-radio-group
-        id="btn-radios-1"
-        v-model="selected_bloodsugar"
-        :options="options_3"
-        buttons
-        name="radios-btn-default"
-      ></b-form-radio-group>
-      <p>
-        bloodsugar State:
-        <strong>{{selected_bloodsugar}}</strong>
-      </p>
-
-      <!--Resting Electrocardiographic Results-->
-
-      <h3
-        class="c-form-label"
-        for="Resting Electrocardiographic Results"
-      >Resting Electrocardiographic Results</h3>
-      <div>
-        <b-form-radio-group
-          id="radio-group-2"
-          v-model="selected_electrocardiographic"
-          :options="options_4"
-          name="radio-options1"
-        ></b-form-radio-group>
-
-        <div class="mt-3">
-          Selected:
-          <strong>{{ selected_electrocardiographic }}</strong>
-        </div>
-      </div>
-
-      <!--Maximum Heart Rate Achieved-->
-      <div role="group">
-        <h3 for="input-live">Maximum Heart Rate Achieved:</h3>
-        <b-form-input
-          id="input-live"
-          v-model="heart_rate"
-          :state="heart_rateState"
-          aria-describedby="input-live-help input-live-feedback"
-          placeholder="Enter your Maximum Heart Rate Achieved"
-          trim
-        ></b-form-input>
-
-        <!-- This will only be shown if the preceding input has an invalid state -->
-        <b-form-invalid-feedback id="input-live-feedback">Please enter Maximum Heart Rate Achieved</b-form-invalid-feedback>
-
-        <!-- This is a form text block (formerly known as help block) -->
-        <!--<b-form-text id="input-live-help">Your age.</b-form-text>-->
-        <p>
-          Input max heart rate:
-          <strong>{{heart_rate}}</strong>
-        </p>
-      </div>
-
-      <!-- Exercise Induced Angina -->
-      <h3 class="c-form-label" for="angina">Exercise Induced Angina</h3>
-      <br>
-      <b-form-radio-group
-        id="btn-radios-1"
-        v-model="selected_angina"
-        :options="options_5"
-        buttons
-        name="radios-btn-default"
-      ></b-form-radio-group>
-      <p>
-        Exercise angina State:
-        <strong>{{selected_angina}}</strong>
-      </p>
-
-      <!-- Old peak  ST depression induced by exercise relative to rest -->
-      <div role="group">
-        <h3 for="input-live">Old peak:ST depression induced by exercise relative to rest:</h3>
-        <b-form-input
-          id="input-live"
-          v-model="oldpeak"
-          :state="oldpeakState"
-          aria-describedby="input-live-help input-live-feedback"
-          placeholder="Enter your Old peak:ST depression induced by exercise relative to rest"
-          trim
-        ></b-form-input>
-
-        <!-- This will only be shown if the preceding input has an invalid state -->
-        <b-form-invalid-feedback
-          id="input-live-feedback"
-        >Please enter Old peak:ST depression induced by exercise relative to rest</b-form-invalid-feedback>
-
-        <!-- This is a form text block (formerly known as help block) -->
-        <!--<b-form-text id="input-live-help">Your age.</b-form-text>-->
-        <p>
-          Input oldpeak:
-          <strong>{{oldpeak}}</strong>
-        </p>
-      </div>
-      <!-- The Slope of the Peak Exercise ST segment -->
-
-      <h3 class="c-form-label" for="slope">The Slope of the Peak Exercise ST segment</h3>
-      <br>
-      <b-form-radio-group
-        id="btn-radios-1"
-        v-model="selected_slope"
-        :options="options_6"
-        buttons
-        name="radios-btn-default"
-      ></b-form-radio-group>
-      <p>
-        Slope of Peak State:
-        <strong>{{selected_slope}}</strong>
-      </p>
-
-      <!-- ca: number of major vessels (0-3) colored by flourosopy  -->
-      <h3 class="c-form-label" for="ca">Number of major vessels (0-3) colored by flourosopy</h3>
-      <br>
-      <b-form-radio-group
-        id="btn-radios-1"
-        v-model="selected_ca"
-        :options="options_7"
-        buttons
-        name="radios-btn-default"
-      ></b-form-radio-group>
-      <p>
-        Number of major vessels:
-        <strong>{{selected_ca}}</strong>
-      </p>
     </div>
-    <br>
-    <b-button @click="onSubmit" type="submit" variant="info">Submit</b-button>
-    <br>
+    <!--Gender selector -->
   </div>
 </template>
 
@@ -351,4 +362,49 @@ export default {
 </script>
 
 <style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+h5 {
+  margin-bottom: 0.5rem;
+  margin-top: 1rem;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+/*{
+  color: #42b983;
+}*/
+
+.aaa {
+  position: middle;
+  text-align: center;
+}
+
+.tile {
+  position: middle;
+  background: rgb(248, 248, 248);
+  border-radius: 3px;
+  padding: 20px;
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12),
+    0 3px 1px -2px rgba(0, 0, 0, 0.2);
+  margin-bottom: 30px;
+  transition: all 0.3s ease-in-out;
+  text-align: left;
+}
+
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: auto;
+  margin-right: auto;
+  align-items: center;
+  justify-content: center;
+}
 </style>
