904a037e1f2d135d008bf3d0296c4011d38442a3
Hao Wu <z5132458@unsw.edu.au>
2019-04-10 04:17:05 +1000

    this is for tutor to test accurancy

--- read_model.py
+++ read_model.py
@@ -1,29 +1,37 @@
 
 # an example to read model from logistic.py
-import pickle
+import pandas as pd
+import numpy as np
+import matplotlib
+from sklearn.multiclass import OutputCodeClassifier
+from sklearn.svm import LinearSVC
 from read_data import *
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.model_selection import train_test_split
-from logistic import *
+from sklearn.svm import SVC
+from sklearn.linear_model import RidgeClassifierCV
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn import linear_model
+from sklearn.linear_model import Perceptron
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV, StratifiedKFold, train_test_split
+from sklearn.naive_bayes import MultinomialNB
+import pickle
 
-##import the data file , here I use original data as an example and use logistic_regression_with_log.sav
-def use_logistic_with_log(modle_file):
+def use_finaly_model_to_test(modle_file):
     loaded_model=pickle.load(open(modle_file,'rb'))
-    # assume we have new dataset,apply the clean_data_function
-    data=clean_data()
-    x=data.drop(['target','pressure','cholestoral','age','heart_rate'],axis=1)
-    y=data['target']
+    raw_frame=thal_data()
+    # name=raw_frame.drop(['sugar','age','cardiographic','angina','slope','thal'],axis=1).columns
+    x=raw_frame.drop(['sugar','age','cardiographic','angina','slope','thal'],axis=1).values
+    y=raw_frame['thal'].values
     result=loaded_model.score(x,y)
     print(result)
-
-
-def use_logistic_without_log(modle_file):
-    loaded_model=pickle.load(open(modle_file,'rb'))
-    # assume we have new dataset,apply the clean_data_function
-    data=clean_data()
-    x=data.drop(['target','log_pressure','log_cholestoral','log_age','log_heart_rate'],axis=1)
-    y=data['target']
-    result=loaded_model.score(x,y)
-    print(result)
-
-use_logistic_with_log('logistic_regression_with_log.sav')
-use_logistic_without_log('logistic_regression_without_log.sav')
+use_finaly_model_to_test('final_model.sav')
