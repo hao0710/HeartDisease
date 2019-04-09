03e04f9a1f53f1d242609ed84082f6b030de5f82
Hao Wu <z5132458@unsw.edu.au>
2019-04-10 04:01:40 +1000

    use this python file for frontend user input

--- use_model.py
+++ use_model.py
@@ -4,18 +4,7 @@ import pickle
 from math import log
 
 # the model use here is logistic_cv_with_log
-def predict_1(age,sex,chest,pressure,cholestoral,sugar,cardiographic,heart_rate,angina,oldpeak,slope,flourosopy,thal):
-    model=pickle.load(open('logistic_regression_with_log.sav','rb'))
-    x=np.array([log(age),sex,chest,log(pressure),log(cholestoral),sugar,cardiographic,log(heart_rate),angina,oldpeak,slope,flourosopy,thal]).reshape(1,13)
-    a=model.predict(x)
-    return a.item(0)
 
-
-def predict_2(age,sex,chest,pressure,cholestoral,sugar,cardiographic,heart_rate,angina,oldpeak,slope,flourosopy,thal):
-    model=pickle.load(open('logistic_regression_without_log.sav','rb'))
-    x=np.array([age,sex,chest,pressure,cholestoral,sugar,cardiographic,heart_rate,angina,oldpeak,slope,flourosopy,thal]).reshape(1,13)
-    a=model.predict(x)
-    return a.item(0)
 
 def predict_3(age,sex,chest,pressure,cholestoral,sugar,cardiographic,heart_rate,angina,oldpeak,slope,flourosopy,thal):
     model=pickle.load(open('logistic_cv_with_log.sav','rb'))
@@ -27,9 +16,10 @@ def predict_4(age,sex,chest,pressure,cholestoral,sugar,cardiographic,heart_rate,
     x=np.array([age,sex,chest,pressure,cholestoral,sugar,cardiographic,heart_rate,angina,oldpeak,slope,flourosopy,thal]).reshape(1,13)
     a=model.predict(x)
     return a.item(0) 
+def predict_final(age,sex,chest,pressure,cholestoral,heart_rate,oldpeak,floursopy):
+    model=pickle.load(open('final_model.sav','rb'))
+    x=np.array([log(age),sex,chest,pressure,log(pressure),cholestoral,log(cholestoral),heart_rate,log(heart_rate),oldpeak,floursopy]).reshape(1,11)
+    a=model.predict(x)
+    return a.item(0)
 
-
-print(predict_1(63,1,1,145,233,1,2,150,0,2.3,3,0,6))
-print(predict_2(63,1,1,145,233,1,2,150,0,2.3,3,0,6))
-print(predict_3(63,1,1,145,233,1,2,150,0,2.3,3,0,6))
-print(predict_4(63,1,1,145,233,1,2,150,0,2.3,3,0,6))
+print(predict_final(63,1,1,145,150,2.3,3,0))
