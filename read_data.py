24436293be4f8212eaafbd3dfc8ca4fddc5e6ec9
Hao Wu <z5132458@unsw.edu.au>
2019-04-10 04:11:13 +1000

    add read thal data

--- read_data.py
+++ read_data.py
@@ -25,3 +25,21 @@ def clean_data():
     df=df[['age','log_age','sex','chest','pressure','log_pressure','cholestoral','log_cholestoral','sugar',\
         'cardiographic','heart_rate','log_heart_rate','angina','oldpeak','slope','flourosopy','thal','target']]
     return df
+def thal_data():
+    path=os.path.abspath('.')+"/data/heart/processed.cleveland.data"
+    chest_pain={'1.0':"typical angin",'2.0':"atypical angina",'3.0':"non-anginal pain","4.0":"asymptomatic"}
+    cardiographic={'0.0':"normal",'1.0':"having ST-T",'2.0':"probable or definite"}
+    thal={'3.0':"normal",'6.0':"fixed defect",'7.0':"reversable defect",'?':"?"}
+    names=['age','sex','chest','pressure','cholestoral','sugar',\
+        'cardiographic','heart_rate','angina','oldpeak','slope','flourosopy','thal','target']        
+    df = pd.read_csv(path, delimiter=',',float_precision='high',header=None,names=names)
+    df=df.drop(df[(df['thal']=='?')|(df['flourosopy']=='?')].index)
+    df=df.drop(columns=['target'],axis=1)
+    df.iloc[:,1]= df.iloc[:,1].map(lambda x: 1 if x == 1 else 0)
+    df['log_pressure'] = np.log(df['pressure'])
+    df['log_cholestoral']=np.log(df['cholestoral'])
+    df['log_age']=np.log(df['age'])
+    df['log_heart_rate']=np.log(df['heart_rate'])
+    df=df[['age','log_age','sex','chest','pressure','log_pressure','cholestoral','log_cholestoral','sugar',\
+        'cardiographic','heart_rate','log_heart_rate','angina','oldpeak','slope','flourosopy','thal']]
+    return df
